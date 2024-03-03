f77280f54b29c24cf51b691d0f605f8819da7f97
alexey1231231230 <117567518+alexey1231231230@users.noreply.github.com>
2024-03-03 20:41:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,33 @@
 'use strict'
 
-let smartphone_1 = {
-    name:'Iphone',
-    image:'./src/images/52ae53268d51c10531162a005f525054.jpg',
-    model:'XR',
-    price:999,
-    battery:5200,
-    RAM:16,
-    ROM:512,
-    weight:450,
-    camera:35,
-}
-
-let smartphone_2 = {
-    name:'Iphone',
-    image:'./src/images/52ae53268d51c10531162a005f525054.jpg',
-    model:'XXXXR',
-    price:999,
-    battery:5200,
-    RAM:32,
-    ROM:512,
-    weight:450,
-    camera:35,
-}
-
 const product_div = document.body
 
 function addSmartphoneToDiv(smartphone, div){
     let div_bg = document.createElement('div')
-    let div_description = document.createElement('div')
+    div_bg.className = 'phone-bg'
+
+    let div_img = document.createElement('div')
+    div_img.className = 'phone-img'
+    div_bg.append(div_img)
+
     let image = document.createElement('img')
     image.src = smartphone.image
+    div_img.append(image)
 
-    let name = document.createElement('h3')
-    name.innerText = smartphone.name
-
-    div_bg.append(image)
+    let div_description = document.createElement('div')
+    div_description.className = 'phone-description'
     div_bg.append(div_description)
 
+    let name = document.createElement('h3')
+    name.innerHTML = `${smartphone.name} <span>${smartphone.model}</span>`
+    
     div_description.append(name)
 
     div.append(div_bg)
 }
 
-addSmartphoneToDiv(smartphone_1, product_div)
+function showPhones( phone ) {
+    addSmartphoneToDiv(phone, product_div)
+}
+
+phones.forEach( showPhones )
